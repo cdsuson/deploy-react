95539c8e50a17cd464de9570ac7432d258c7b2c8
Christine Suson <christinesuson@Christines-MacBook-Pro.local>
2020-10-01 15:33:27 +0800

    Updates

---
+++ precache-manifest.0e633a99c87056fc5ea0304c2d7800a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c427c05c648010684e479a89b85820",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "a973aef772094d8f1bdf",
+    "url": "/deploy-react/static/css/main.ace16608.chunk.css"
+  },
+  {
+    "revision": "85394a843ed82a485fea",
+    "url": "/deploy-react/static/js/2.e34e154a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.e34e154a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a973aef772094d8f1bdf",
+    "url": "/deploy-react/static/js/main.198a7525.chunk.js"
+  },
+  {
+    "revision": "dcee89ea846fb33ddfa1",
+    "url": "/deploy-react/static/js/runtime-main.e42979b5.js"
+  }
+]);
